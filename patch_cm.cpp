9900958fb1f7b7f995351eae153ff3d306a81c67
B0rus1a <borrussiagame@mail.ru>
2026-07-04 18:40:44 +0300

    Refactor project structure

--- patch_cm.cpp
+++
@@ -1,5 +0,0 @@
-#include "CareerManager.h"
-#include "GameManager.h"
-#include "Player.h"
-
-// Note: This is a patch file containing only the new methods. I will use multi_replace_file_content to insert them.
